292e6f06256d07de69a16c80c97e508906e43cbf
Carlos Freitas <77859843+EngCarlos-ELT@users.noreply.github.com>
2024-11-28 15:09:21 -0300

    Update Learning_Python_v2.py

--- Learning_Python_v2.py
+++ Learning_Python_v2.py
@@ -16,7 +16,7 @@ into other modules, You can use the special __all__ attribute in __init__.py to 
 from pacote import * .
 relative import
 prospector
-memory profiler
+memory profiler, timeit, %timeit, repeat
 """
 import time
 
